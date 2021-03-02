b173d439860ae29f00ce8cee41f4e766df90cecc
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2021-03-03 07:59:54 +0900

    続き

--- nextjs-blog/pages/01.tsx
+++ nextjs-blog/pages/01.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
 
+
 type Data = {
-    message: string;
-    status: string;
+  message: string[];
+  status: string;
 };
 
-const HelloFetchImage: React.FC = () => {
-    const [imageUrl, setImageUrl] = React.useState('');
-    const fetchData = async () => {
-        try {
-            const url = 'https://dog.ceo/api/breeds/image/random';
-            const res = await fetch(url);
-            const data: Data = await res.json();
-            setImageUrl(data.message);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+type Props = {
+  imageNums: number;
+};
 
-    React.useEffect(() => {
-        fetchData();
-    },[]);
+const HelloFetchImages: React.FC<Props> = (props) => {
+  const [imageUrls, setImageUrls] = React.useState<string[]>([]);
+  const [loading, setLoading] = React.useState(false);
 
-    return (
-        <div>
-            {imageUrl && <img src={imageUrl}/>}
-            <div>
-                <button onClick={fetchData}>fetch</button>
-                <button onClick={()=>setImageUrl('')}>clear</button>
-            </div>
-        </div>
-    );
+  // データ取得とステートの保持
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const url = `https://dog.ceo/api/breeds/image/random/${props.imageNums}`;
+      const res = await fetch(url);
+      const data: Data = await res.json();
+      setImageUrls(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
 
+  // 副作用フック（マウント時）
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading
+        ? 'loading....'
+        : imageUrls.map((x) => (
+            <img key={x} src={x} style={{ width: 200, height: 200 }} />
+          ))}
+      <div>
+        <button onClick={fetchData}>fetch</button>
+        <button onClick={() => setImageUrls([])}>clear</button>
+      </div>
+    </div>
+  );
 };
 
 export default function Index() {
-    return(
-        <div>
-            <HelloFetchImage/>
-        </div>
-    )
+  return (
+    <div>
+      <HelloFetchImages imageNums={20} />
+    </div>
+  );
 }
